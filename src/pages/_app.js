b118d7da499ea5875cb7dfd6f54bb0a1fe749582
chocoscoding <chocoscoding@gmail.com>
2024-03-16 17:19:03 +0100

    project init

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,45 @@
 import "@/styles/globals.css";
+import localFont from 'next/font/local';
 
+// Font files can be colocated inside of `pages`
+const myFont = localFont({
+  src: [
+    {
+      path: '../fonts/PlainLight.otf',
+      weight: '200',
+      style: 'normal',
+    },
+    {
+      path: '../fonts/PlainLightItalic.otf',
+      weight: '200',
+      style: 'italic',
+    },
+    {
+      path: '../fonts/PlainRegular.otf',
+      weight: '400',
+      style: 'normal',
+    },
+    {
+      path: '../fonts/PlainRegularItalic.otf',
+      weight: '400',
+      style: 'italic',
+    },
+    {
+      path: '../fonts/PlainUltrabold.otf',
+      weight: '700',
+      style: 'normal',
+    },
+    {
+      path: '../fonts/PlainUltraBoldItalic.otf',
+      weight: '700',
+      style: 'italic',
+    },
+  ],
+});
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (<main className={myFont.className}>
+
+    <Component {...pageProps} />
+  </main>
+  );
 }
---
+++ src/pages/index.jsx
@@ -0,0 +1,41 @@
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import SplitText from "gsap/dist/SplitText";
+gsap.registerPlugin(SplitText);
+
+export default function Home() {
+  const nameSectionTl = new gsap.timeline();
+  const nameSectionTl2 = new gsap.timeline();
+  useGSAP(() => {
+    let text = new SplitText(" .nameSpan1", { type: "chars" });
+    let text2 = new SplitText(" .nameSpan2", { type: "chars" });
+    let chars = text.chars;
+    let chars2 = text2.chars;
+
+    gsap.from(chars, {
+      yPercent: -300,
+      stagger: 0.05,
+      ease: "back-out",
+      duration: 0.4,
+    });
+    gsap.from(chars2, {
+      yPercent: -120,
+      stagger: 0.05,
+      ease: "back-out",
+      delay: 0.4,
+      duration: 0.4,
+    });
+  }, []);
+  return (
+    <main>
+      <section className="nameSection">
+        <div className="firstName">
+          <p className="nameSpan nameSpan1">Oyeti</p>
+        </div>
+        <div className="lastName">
+          <p className="nameSpan nameSpan2">Oluwatimileyin</p>
+        </div>
+      </section>
+    </main>
+  );
+}
